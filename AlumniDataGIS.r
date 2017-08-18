91e41a918219f9324defc5d4d48175d1935d688d
vidhi0591 <vidhi0591@gmail.com>
2017-08-18 12:42:26 -0500

    Create AlumniDataGIS.r

---
+++ AlumniDataGIS.r
@@ -0,0 +1,2 @@
+#this is the alumni data for the TAMUC GIS Alumni Outreach project, which is written in R.
+#This code is licensed under the GPL version 2, for more information, please contact fsf.org.
